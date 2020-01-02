72e0f37edbe7c96955b2d857771099874e575dac
diaojt <diaojt500@gmail.com>
2020-01-02 15:04:19 +0800

    初始化信息

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by Smy_D on 2020/1/2.
+//  Copyright © 2020 Smy_D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
